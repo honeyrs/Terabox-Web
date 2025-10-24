592de47baf391146112efd84ee6e7834427f5d5c
honeyrs <rsnaam00@gmail.com>
2025-10-25 01:53:53 +0530

    Update and rename save.py to save1.py

--- save1.py
+++ save1.py
@@ -1,6 +1,6 @@
-# /root/TeamUltroid/addons/save.py
-from pyUltroid.fns.tools import ultroid_cmd  # Correct import
-from . import eod  # Assuming eod is defined in addons/__init__.py or elsewhere
+# /root/TeamUltroid/addons/save (1).py
+from pyUltroid.fns.tools import ultroid_bot as ultroid_cmd  # Alias ultroid_bot to ultroid_cmd
+from pyUltroid.fns.helper import eod  # Ensure eod is imported correctly
 
 async def get_reply_chain(message, client):
     """Recursively collect all messages in the reply chain."""
